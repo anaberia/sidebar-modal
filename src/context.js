b372270b79902e03062e46fd4835723e70957f72
anaberia <ana.beria3@gmail.com>
2022-11-15 19:20:35 +0400

    add states and functions on context components

--- src/context.js
+++ src/context.js
@@ -3,7 +3,39 @@ import React, { useState, useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="Hello">{children}</AppContext.Provider>;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={
+        (modalOpen,
+        sidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar)
+      }
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
