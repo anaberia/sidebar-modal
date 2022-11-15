d47b2e12248b8b27872118f889797e3cf20e1022
anaberia <ana.beria3@gmail.com>
2022-11-15 17:24:02 +0400

    add main buttons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
-import { social, links } from ".data";
+import { social, links } from "./data";
 
 const Sidebar = () => {
   return <div>Sidebar</div>;
